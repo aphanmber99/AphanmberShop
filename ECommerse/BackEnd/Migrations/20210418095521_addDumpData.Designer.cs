251cbc8bf7f5c037c79d20c9897df4990203fd0f
Aphanmber <aphanmber99@gmail.com>
2021-04-18 17:01:45 +0700

    Add DumpData

--- ECommerse/BackEnd/Migrations/20210418095521_addDumpData.Designer.cs
+++ ECommerse/BackEnd/Migrations/20210418095521_addDumpData.Designer.cs
@@ -4,14 +4,16 @@ using BackEnd.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BackEnd.Migrations
 {
     [DbContext(typeof(AplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210418095521_addDumpData")]
+    partial class addDumpData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
