d75b073e96e6c861a5611468b48c506d66ae570e
Aphanmber <aphanmber99@gmail.com>
2021-04-18 17:03:03 +0700

    add product preview view component

---
+++ ECommerse/CustomerSite/Views/Shared/Components/ProductPreview/ProductPreview.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using CustomerSite.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Shared.ViewModel;
+
+namespace CustomerSite.Views.Shared.Components.ProductPreview
+{
+    [ViewComponent]
+    public class ProductPreview : ViewComponent
+    {
+        
+        private readonly IHttpClientFactory _httpFactory;
+        
+        public ProductPreview(IHttpClientFactory httpFactory)
+        {
+            _httpFactory = httpFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync( int categoryId, string titlePreview)
+        {
+            var queryString = RequestHelper.GetQueryString(categoryId,8);
+            var _httpCleint = _httpFactory.CreateClient("host");
+
+            var resp = await  _httpCleint.GetAsync("product"+queryString);
+            if(!resp.IsSuccessStatusCode) throw new Exception();
+            var reuslt = await resp.Content.ReadFromJsonAsync<List<ProductVM>>();
+            //s
+            ViewBag.TitlePreview = titlePreview;
+            return View(reuslt);
+        }
+    }
+}
