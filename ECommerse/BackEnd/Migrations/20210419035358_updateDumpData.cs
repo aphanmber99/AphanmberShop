5608b67896ba1034b185d4c17cd758a888945f5d
Aphanmber <aphanmber99@gmail.com>
2021-04-19 11:33:23 +0700

    Add dump data

---
+++ ECommerse/BackEnd/Migrations/20210419035358_updateDumpData.cs
@@ -0,0 +1,349 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class updateDumpData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 1,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 2,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 3,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 5,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 6,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 7,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 8,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 9,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 10,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 11,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 12,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 13,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 14,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 15,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 16,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 17,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 18,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 19,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 20,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 21,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 22,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 23,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 24,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 25,
+                column: "proDescription",
+                value: "Product Summary/Product Desscription");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 1,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 2,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 3,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 5,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 6,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 7,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 8,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 9,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 10,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 11,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 12,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 13,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 14,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 15,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 16,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 17,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 18,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 19,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 20,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 21,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 22,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 23,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 24,
+                column: "proDescription",
+                value: "Product Desscription");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "proID",
+                keyValue: 25,
+                column: "proDescription",
+                value: "Product Desscription");
+        }
+    }
+}
