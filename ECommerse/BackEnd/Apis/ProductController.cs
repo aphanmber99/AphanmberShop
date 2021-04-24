aba142261d79e14dfdde53d2af87434b8174e117
Aphanmber <aphanmber99@gmail.com>
2021-04-24 17:35:10 +0700

    Add upload image

--- ECommerse/BackEnd/Apis/ProductController.cs
+++ ECommerse/BackEnd/Apis/ProductController.cs
@@ -9,6 +9,10 @@ using AutoMapper;
 using BackEnd.Interface;
 using System.Threading.Tasks;
 using Shared.Enum;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System;
 
 namespace BackEnd.Controllers
 {
@@ -17,52 +21,88 @@ namespace BackEnd.Controllers
     public class ProductController : ControllerBase
     {
         private IProductService _productSer;
-        public ProductController(IProductService productService){
+        public ProductController(IProductService productService)
+        {
             _productSer = productService;
         }
 
         [HttpGet]
-        public async Task<List<ProductVM>> GetListAsync(int categoryId, SortProduct? sort = null, int pageSize = 0, int page = 0){
-            var re = await _productSer.GetListAsync(categoryId,sort,pageSize,page);
-            HttpContext.Response.Headers.Add("total-item",re.totalItem.ToString());
+        public async Task<List<ProductVM>> GetListAsync(int categoryId, SortProduct? sort = null, int pageSize = 0, int page = 0)
+        {
+            var re = await _productSer.GetListAsync(categoryId, sort, pageSize, page);
+            HttpContext.Response.Headers.Add("total-item", re.totalItem.ToString());
             return re.products;
         }
 
         [HttpGet("search")]
-        public async Task<List<ProductVM>> SearchAsync(string query, SortProduct? sort = null, int pageSize = 0, int page = 0){
-             var re = await _productSer.SearchAsync(query,sort,pageSize,page);
-            HttpContext.Response.Headers.Add("total-item",re.totalItem.ToString());
+        public async Task<List<ProductVM>> SearchAsync(string query, SortProduct? sort = null, int pageSize = 0, int page = 0)
+        {
+            var re = await _productSer.SearchAsync(query, sort, pageSize, page);
+            HttpContext.Response.Headers.Add("total-item", re.totalItem.ToString());
             return re.products;
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ProductVM>> GetAsync(int id){
-           if(id<=0) return BadRequest();
-           var result = await _productSer.GetAsync(id);
-           return result;
+        public async Task<ActionResult<ProductVM>> GetAsync(int id)
+        {
+            if (id <= 0) return BadRequest();
+            var result = await _productSer.GetAsync(id);
+            return result;
         }
 
-        [HttpDelete("{id}")]     
+        [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteAsync(int id)
-        {    
-            if(id<=0) return BadRequest();
-             var result = await _productSer.DeleteAsync(id);
+        {
+            if (id <= 0) return BadRequest();
+            var result = await _productSer.DeleteAsync(id);
             return Ok();
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateAsync(int id, ProductVM productVM )
-        {    
-            if(id != productVM.proID) return BadRequest();
-             var result = await _productSer.UpdateAsync(id, productVM);
+        public async Task<ActionResult> UpdateAsync([FromServices] IWebHostEnvironment env, int id, [FromForm] ProductVM productVM, IFormFile image)
+        {
+            if (id != productVM.proID) return BadRequest();
+            if (image != null)
+            {
+                var filePath = Path.Combine(env.WebRootPath, "images", image.FileName);
+                try
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await image.CopyToAsync(stream);
+                    }
+                    productVM.Image = image.FileName;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Can't upload image");
+                }
+            }
+            var result = await _productSer.UpdateAsync(id, productVM);
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVM>> CreateAsync(ProductVM productVM)
+        public async Task<ActionResult<ProductVM>> CreateAsync([FromServices] IWebHostEnvironment env,[FromForm] ProductVM productVM, IFormFile image)
         {
-            if(!ModelState.IsValid) return BadRequest();
-             var result = await _productSer.CreateAsync(productVM);
+            if (!ModelState.IsValid) return BadRequest();
+            if (image != null)
+            {
+                var filePath = Path.Combine(env.WebRootPath, "images", image.FileName);
+                try
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await image.CopyToAsync(stream);
+                    }
+                    productVM.Image = image.FileName;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Can't upload image");
+                }
+            }
+            var result = await _productSer.CreateAsync(productVM);
             return result;
         }
 
